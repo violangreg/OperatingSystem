8fd84a47ee7aee98becaa3e89b775c56f5749a7a
violangreg <violan.greg@gmail.com>
2017-03-12 10:11:59 -0700

    sec

--- project2/project2.c
+++ project2/project2.c
@@ -1,58 +1,66 @@
 /**************************************************************************/
-/* Name: Greg Paolo Violan and Luke Nguyen
+/* Name: Greg Paolo Violan and Luke Nguyen	DATE: 3/12/17
 /* PROGRAM: project2.c */
 /* DESCRIPTION: Parent makes children and children does arithmetic functions*/
 /**************************************************************************/
+// importing proper includes
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <unistd.h>
 
-
 int main(int argc, char *argv[])
 {
+	// declaring sleeptime = how many seconds to sleep (wait) the program
+	// declaring seed = for randomizing sleeptime
+	// delcaring chilpid = for getting the value of the child's pid
 	int sleeptime, seed;
-	
-	pid_t childpid; // declaring children variables & children template
+	pid_t childpid;
 
-	// Checks input arguments
+	// checks input arguments
 	if (argc != 4) 
 	{
 		printf("Usage: %s sleep int int \n", argv[0]);
 		exit(1);
 	}
 	
+	// assigning argument 1 from command line to sleeptime
 	sleeptime = atoi(argv[1]); //
 	
-	// Input checker
+	// input checker for sleeptime
 	if(sleeptime < 0 || sleeptime > 50)
 	{
 		printf("Error, sleeptime is out of bounds (0-50)\n");
 		exit(1);
 	}
 
-	// Forking and creating a children tree up to 'nth' level
+	// forking and creating 4 childrens
 	for(int i = 0; i < 4; i++)
 	{
+		/* using i as the key to figure out which case statement to go into on the child program 
+		converting i to char since execve can only take char array
+		*/
 		char temp_i = (char) i + '0';
 		char *new_argv[] = {"./project2b", &temp_i, argv[1], argv[2], argv[3], NULL};
 		
+		// forking and exec'ing, passing in argv array with proper arguments  
 		if((childpid = fork()) == 0)
 		{
+			// find project2b file and passing in new_argv as its command line starting with ./project2b as argv[0] ... and so forth
 			int ret = execve("project2b", new_argv, NULL);
 			perror("Exec failure");
 			exit(1);
 		}
 	} 
 
-	// Check if the fork failed
+	// check if the fork failed
 	if(childpid < 0)
 	{
 		perror("The fork failed \n");
 		exit(1);
 	}
 
-	// Display output
+	// display parent output
 	printf("I am the process parent, the maximum sleep time is %d and the two numbers are %d and %d \n", sleeptime, atoi(argv[2]), atoi(argv[3])); 
 	
 	/* since each process has a different childpid, using the childpid
@@ -68,6 +76,4 @@ int main(int argc, char *argv[])
 	sleep(s);
 	exit(0);
 }
-
-// End of main
-
+// end of main
--- project2/project2b.c
+++ project2/project2b.c
@@ -1,24 +1,29 @@
 /**************************************************************************/
-/* Name: Greg Paolo Violan and Luke Nguyen
+/* Name: Greg Paolo Violan and Luke Nguyen	DATE: 3/12/17
 /* PROGRAM: project2.c */
-/* DESCRIPTION: Parent makes children and children does arithmetic functions*/
+/* DESCRIPTION: This is the child class, it does the arithmetic functions*/
 /**************************************************************************/
+// importing proper includes
 # include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <unistd.h>
 
-
 int main(int argc, char *argv[])
 {
+	// declaring all necessary variables
+	// i = key, sleeptime = sleep, n1 = 1st integer (a), n2 = 2nd integer (b), seed = randomization sleep
+	// sum = a + b, diff = a - b, mult = a * b, div = a / b
 	int i, sleeptime, n1, n2, seed;
 	int sum, diff, mult, div;
 	
+	// assigning argv to proper variables note argv[0] is ./project2b which is calling to run this program
 	i = atoi(argv[1]);
 	sleeptime = atoi(argv[2]);
 	n1 = atoi(argv[3]);
 	n2 = atoi(argv[4]);
 
+	// case statements for all the arithmetic functions using the key i
 	switch(i)
 	{
 		case 0:
@@ -36,13 +41,12 @@ int main(int argc, char *argv[])
 			break;
 		case 3:
 			if(n2 == 0)
+				printf("I am child 3 with PID %d, error: sec int is 0 \n", getpid());
+			else
 			{
-				printf("Error: second int is 0");
-				exit(1);
+				div = n1 / n2;
+				printf("I am child 3 with PID %d, the divided is %d \n", getpid(), div);
 			}
-			
-			div = n1 / n2;
-			printf("I am child 3 with PID %d, the divided is %d \n", getpid(), div);
 			break;
 		default: printf("case: default \n");
 			break;
@@ -61,6 +65,4 @@ int main(int argc, char *argv[])
 	sleep(s);
 	exit(0);
 }
-
-// End of main
-
+// end of main
